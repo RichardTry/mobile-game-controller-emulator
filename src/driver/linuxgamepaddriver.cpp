6922830d85ec95e7f53fdf275937a053c252284a
Muhammet Fevzi Bayıroğlu <fevzi-bayiroglu@yandex.com>
2020-12-07 23:12:09 +0300

    Some simple cleanup

--- src/driver/linuxgamepaddriver.cpp
+++ src/driver/linuxgamepaddriver.cpp
@@ -8,31 +8,38 @@ LinuxGamepadDriver::LinuxGamepadDriver(QObject *parent): AbstractDriver(parent) 
 void LinuxGamepadDriver::onDataArrived(const QByteArray &data) {
     GamepadController::GamepadEvent event(data);
 
+    const int infoTextWidth = 20;
+    const int numberWidth = 15;
     const QString btnLabel = labelForButton(event.m_button);
     switch (event.m_type) {
     case GamepadController::GamepadEvent::ButtonPressEvent: {
-        qDebug() << QString("Button pressed:").leftJustified(50) << btnLabel.rightJustified(10, ' ');
+        qDebug() << QString("Button pressed:").leftJustified(infoTextWidth) << btnLabel.rightJustified(numberWidth, ' ');
+        break;
     }
     case GamepadController::GamepadEvent::ButtonReleaseEvent: {
-        qDebug() << QString("Button released:").leftJustified(50) << btnLabel.rightJustified(10, ' ');
+        qDebug() << QString("Button released:").leftJustified(infoTextWidth) << btnLabel.rightJustified(numberWidth, ' ');
+        break;
     }
     case GamepadController::GamepadEvent::StickMoveEvent: {
-        qDebug() << QString(btnLabel + " Moved:").leftJustified(50)
-                 << QString::number(event.m_value.x()).rightJustified(10)
+        qDebug() << QString(btnLabel + " Moved:").leftJustified(infoTextWidth)
+                 << QString::number(event.m_value.x()).rightJustified(numberWidth)
                  << " "
-                 << QString::number(event.m_value.y()).rightJustified(10);
+                 << QString::number(event.m_value.y()).rightJustified(numberWidth);
+        break;
     }
     case GamepadController::GamepadEvent::StickPressEvent: {
-        qDebug() << QString(btnLabel + " Pressed:").leftJustified(50)
-                 << QString::number(event.m_value.x()).rightJustified(10)
+        qDebug() << QString(btnLabel + " Pressed:").leftJustified(infoTextWidth)
+                 << QString::number(event.m_value.x()).rightJustified(numberWidth)
                  << " "
-                 << QString::number(event.m_value.y()).rightJustified(10);
+                 << QString::number(event.m_value.y()).rightJustified(numberWidth);
+        break;
     }
     case GamepadController::GamepadEvent::StickReleaseEvent: {
-        qDebug() << QString(btnLabel + " Released:").leftJustified(50)
-                 << QString::number(event.m_value.x()).rightJustified(10)
+        qDebug() << QString(btnLabel + " Released:").leftJustified(infoTextWidth)
+                 << QString::number(event.m_value.x()).rightJustified(numberWidth)
                  << " "
-                 << QString::number(event.m_value.y()).rightJustified(10);
+                 << QString::number(event.m_value.y()).rightJustified(numberWidth);
+        break;
     }
 
     }
