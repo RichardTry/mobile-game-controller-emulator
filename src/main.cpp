e5e8a43a02324ebf52c604e715893a5492882de4
Muhammet Fevzi Bayıroğlu <fevzi-bayiroglu@yandex.com>
2020-12-08 17:16:15 +0300

    Connect quit buttons to qapp quit, add some comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <QApplication>
 #include <QThread>
+// Common to both
 #include "transceiver/network/networktransceiver.h"
 #include "widget/networktransceiverwidget.h"
-#include "emulator/androidcontrolleremulator.h"
-#include "controller/gamepadcontroller.h"
+// Driver Side
 #include "emulator/genericdriveremulator.h"
 #include "driver/linuxgamepaddriver.h"
+// Controller Side
+#include "emulator/androidcontrolleremulator.h"
+#include "controller/gamepadcontroller.h"
 
 class NetworkWorker: public QThread {
 public:
@@ -31,6 +34,7 @@ int main(int argc, char **argv) {
 #if defined(DRIVER)
     NetworkWorker worker(AbstractTransceiver::Mode::Slave);
     AbstractTransceiver *transceiver = worker.networkTransceiver();
+    QObject::connect(transceiver, &AbstractTransceiver::quit, &app, &QApplication::quit);
     AbstractDriver *driver = new LinuxGamepadDriver;
     GenericDriverEmulator *drivemu = new GenericDriverEmulator(driver, transceiver);
     NetworkTransceiverWidget widget((NetworkTransceiver*)transceiver);
@@ -44,6 +48,7 @@ int main(int argc, char **argv) {
     AndroidControllerEmulator *conemu = new AndroidControllerEmulator(transceiver, controller);
     QObject::connect(transceiver, &AbstractTransceiver::connected, &comWidget, &QWidget::hide);
     QObject::connect(transceiver, &AbstractTransceiver::connected, conemu, &QWidget::show);
+    QObject::connect(transceiver, &AbstractTransceiver::quit, &app, &QApplication::quit);
 #endif
 
     return app.exec();
